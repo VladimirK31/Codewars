6dc1505692bd4314be17894b0d218070090fc2ba
VladimirK31 <93672836+VladimirK31@users.noreply.github.com>
2022-03-08 16:54:58 +0300

    5.2

--- 5.js
+++ 5.js
@@ -15,3 +15,11 @@ const areaOrPerimeter = function (l, w) {
     return l * 2 + w * 2
   }
 }
+
+function solution(str) {
+  let newStr = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i]
+  }
+  return newStr
+}
