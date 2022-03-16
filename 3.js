7e4c75defade53b8553b71d456323d07152f0a32
VladimirK31 <93672836+VladimirK31@users.noreply.github.com>
2022-03-16 13:36:39 +0300

    add

--- 3.js
+++ 3.js
@@ -6,3 +6,11 @@ function removeChar(str) {
   let arr = str.replace(/^.|.$/g, '')
   return arr
 }
+
+function multiplyAll(arr){
+  return function myFun(num){
+    return arr.map(function(elem){
+      return elem*num
+    })
+  }
+}
